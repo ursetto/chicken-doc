0ff4720570d340f2bb52aefd7f9ade00279ad18a
Jim <jim@amaranth.xorinia.dim>
2009-11-21 09:59:53 -0600

    start parsing sections

--- cdoc-test.scm
+++ cdoc-test.scm
@@ -12,11 +12,17 @@
                (submatch (?? (+ any)))
                "</" (backref 1) ">"
                eol)))
+(define +rx:section+
+  (irregex '(: bol
+               (submatch (** 2 5 "="))
+               (* space)
+               (submatch (+ any))
+               eol)))
 
 ;; Convert signature (usually a list or bare identifier) into an identifier
 ;; At the moment, this just means taking the car of a list if it's a list,
 ;; and if it cannot be read as a scheme expression, fail
-(define (signature->identifier-string sig type)
+(define (signature->identifier sig type)
   (condition-case
    (let ((L (with-input-from-string sig read)))
      (cond ((pair? L) (car L))
@@ -25,22 +31,36 @@
     (warning "Could not parse signature" sig)
     #f)))
 
-(define (check-line line)
+(define (tag-line line)
   (match (string-search +rx:tag+ line)
          ((_ type signature)
-          (print "type: " type " sig: " signature
-                 " id: " (signature->identifier-string signature type)))
+          (list type signature (signature->identifier signature type)))
+         (#f #f)))
+(define (section-line line)
+  (match (string-search +rx:section+ line)
+         ((_ num title)
+          (list num title))
          (#f #f)))
 
 (define (check-all fn)
   (with-input-from-file fn
     (lambda ()
-      (let loop ((line (read-line)))
+      (let loop ((line (read-line))
+                 (section 1))
         (cond ((eof-object? line)
                #f)
+              ((tag-line line) =>
+               (match-lambda ((type sig id)
+                         (print "type: " type " sig: " sig
+                                " id: " id)
+                         (loop (read-line) section))))
+              ((section-line line) =>
+               (match-lambda ((num title)
+                         (print "section: " num " title: " title)
+                         (loop (read-line) section))))
               (else
-               (check-line line)
-               (loop (read-line))))))))
+               (loop (read-line) section))
+              )))))
 
 
 ;; (check-line "<procedure>(abc def)</procedure>")
